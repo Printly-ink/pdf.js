ca58a94bebec648bda08f98348efa2a5a209d2e8
João Vinicius de Paula <joaoviniciusdp@gmail.com>
2024-12-04 18:18:47 -0300

    Install pdfjs-build-generic in react-app

--- react-app/next.config.ts
+++ react-app/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "pdfjs-build-generic": path.resolve(__dirname, "node_modules/pdfjs-build-generic"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
